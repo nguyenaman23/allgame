a913f0c0a08cf85a1f480fd470b5f83e410570dc
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-12-15 13:07:34 +0700

    Update and rename README.md to hngaming.lua

---
+++ hngaming.lua
@@ -0,0 +1,22 @@
+ local placeId = game.PlaceId
+ if placeId == 4520749081 then
+    kingle = true
+ elseif placeId == 6381829480 then
+	kingle = true
+ elseif placeId == 5931540094 then
+     kingle = true
+ end
+ if placeId == 2753915549 then
+		bloxfruit = true
+	elseif placeId == 4442272183 then
+		bloxfruit = true
+	elseif placeId == 7449423635 then
+		bloxfruit = true
+	end
+
+	if bloxfruit then
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/sadwawin/Blox-Fruit/main/README.md", true))()
+	end
+	if kingle then
+		loadstring(game:HttpGet"https://raw.githubusercontent.com/sadwawin/KINGLAGACY/main/README.md")()
+	end
